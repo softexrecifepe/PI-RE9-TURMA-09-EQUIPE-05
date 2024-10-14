510407bd10a9d0e6132acc2e3ad11d919428cee1
Willian Gabriel <willianlopes20@hotmail.com>
2024-10-13 23:09:00 -0300

    criado pasta componentes(header, container e footer), add variaveis das cores e fonte

---
+++ src/components/Header/header.tsx
@@ -0,0 +1,13 @@
+export default function Header () {
+    return (
+        <header>
+            <span className="w-full h-20 bg-red-600">Empregue</span>
+            <nav>
+                <a href="#">texto 1</a>
+                <a href="#">texto 2</a>
+                <a href="#">texto 3</a>
+                <a href="#">texto 4</a>
+            </nav>
+        </header>
+    )
+}
