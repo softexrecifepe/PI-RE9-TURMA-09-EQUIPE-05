139a224f135bbf887d6dae47447772701ec6b694
Willian Gabriel <willianlopes20@hotmail.com>
2024-10-24 15:21:29 -0300

    coloquei as cores do hoover nos links, coloquei a cor na logo e criei o icone que leva para a página de login

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,13 +1,19 @@
+import { FaUser } from "react-icons/fa";
+
 export default function Header(){
     return (
-        <header className="w-full h-16 bg-secondary flex items-center justify-around">
-            <span className="text-2xl font-bold">Empregue</span>
+        <header className="w-full bg-corHeader h-14 flex items-center justify-around">
+            <span className="text-2xl text-cordestaqueTitulo font-bold cursor-pointer">Empregue</span>
             <nav>
-                <a href="#" className="px-6 hover:bg-terceary">Home</a>
-                <a href="#" className="px-6 hover:bg-terceary">Sobre</a>
-                <a href="#" className="px-6 hover:bg-terceary">Cadastre-se</a>
-                <a href="#" className="px-6 hover:bg-terceary">Contato</a>
+                <a href="#" className="hover:text-corSecundaria cursor-pointer tracking-widest ml-5 transition ease-linear duration-200">HOME</a>
+                <a href="#" className="hover:text-corSecundaria cursor-pointer tracking-widest ml-5 transition ease-linear duration-200">SOBRE</a>
+                <a href="#" className="hover:text-corSecundaria cursor-pointer tracking-widest ml-5 transition ease-linear duration-200">VAGAS</a>
+                <a href="#" className="hover:text-corSecundaria cursor-pointer tracking-widest ml-5 transition ease-linear duration-200">CADASTRO</a>
+                <a href="#" className="hover:text-corSecundaria cursor-pointer tracking-widest ml-5 transition ease-linear duration-200">CONTATO</a>
             </nav>
+            <div className="text-corDestaquePricipal text-lg flex items-center hover:text-corSecundaria cursor-pointer">
+                <i><FaUser /></i>
+            </div>
         </header>
     )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,16 +12,15 @@ const config: Config = {
         sans: ['var(--font-family-main)']
       },
       colors: {
-        primary: '#Ad0006',
-        secondary: '#FF0009',
-        terceary: '#Ff6166' ,
-        corDestaque: #ff9ea1,
-        corDestaquePricipal: '#3f3011',
-        corDestaquePricipaMedial: '#534527',
-        corDestaqueHover: '#7a7262',
-        corHeader: '#fffaf1',
-        corVerde: '#51fc02',
-
+        corPrimaria:'#ad0006',
+        cordestaqueTitulo:'#ff0009',
+        corSecundaria:'#ff6166',
+        corDestaque:'#ff9ea1',
+        corDestaquePricipal:'#3f3011',
+        corDestaquePricipalMedial:'#534527',
+        corDestaqueHover:'#7a7262',
+        corHeader:'#fffaf1',
+        corVerde:'#51fc02',
     
       },
     },
