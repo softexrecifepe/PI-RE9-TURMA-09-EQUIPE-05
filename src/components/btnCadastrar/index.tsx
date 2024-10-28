212a92eae0c06e87c199f2f37612e679e12a29fc
MARIA GRACILENE DIAS <MARIA0425@IOT>
2024-10-28 14:37:23 -0300

    criacao paginas vagas e cadastro

---
+++ src/components/btnCadastrar/index.tsx
@@ -0,0 +1,7 @@
+export default function BtnCadastrar() {
+  return (
+    <button className='bg-[var(--cor-primaria)] py-3 flex items-center justify-center rounded-full cursor-pointer mt-5 w-full border-none hover:bg-[var(--cor-destaque-titulo)]'>
+      <span className='text-aliceblue text-lg'>Cadastrar</span>
+    </button>
+  );
+}
---
+++ src/components/txtComoCriar/index.tsx
@@ -0,0 +1,14 @@
+export default function ComoCriar() {
+  return (
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed volutpat
+      mollis venenatis. Praesent tellus tortor, bibendum hendrerit convallis
+      nec, luctus eget magna. Ut ultricies, risus sed consectetur maximus, odio
+      felis malesuada metus, vitae auctor enim odio vitae dolor. Maecenas
+      mollis, magna et maximus posuere, elit diam volutpat nisi, vel consectetur
+      est quam sit amet libero. Quisque et lorem et ante congue gravida ac a
+      lectus. Morbi tristique rutrum tortor quis sollicitudin. Mauris porttitor
+      nisi at felis posuere varius. Maecenas eget lacus lacus. Pellentesque.
+    </p>
+  );
+}
