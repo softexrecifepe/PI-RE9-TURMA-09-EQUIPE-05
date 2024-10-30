21238059896180dccbe6c7f6330de47eddf7726e
WILLIAN GABRIEL LOPES DA SILVA <WILLIAN6423@IOT>
2024-10-30 15:06:38 -0300

    criacao da pagina login e vagas disponiveis

--- src/components/container/index.tsx
+++
@@ -1,7 +0,0 @@
-export default function Container({children}) {
-    return (
-        <section className="w-full min-h-90vh flex flex-col items-center py-4 gap-4">
-            {children}
-        </section>
-    )
-}
