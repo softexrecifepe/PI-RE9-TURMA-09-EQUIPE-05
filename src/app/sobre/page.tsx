282af358032fc4d33972f0bb7926ab9830021904
Willian Gabriel <willianlopes20@hotmail.com>
2024-11-01 20:22:52 -0300

    fixes

--- src/app/sobre/page.tsx
+++ src/app/sobre/page.tsx
@@ -6,21 +6,26 @@ import Image from "next/image";
 export default function Sobre() {
     return (
         <>
-            <Header />            
+            <Header />
             <div className="pt-14 pl-8 min-h-screen flex flex-wrap w-full bg-custom-gradient">
-                    <div className="w-1/2 text-justify px-8">
-                        <h1 className="text-2xl font-bold text-corSecundaria ">Sobre</h1>
-                        <p className="text-corDestaquePricipal">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deserunt ipsam adipisci natus deleniti tempora amet eligendi quo repudiandae! Ipsum consequuntur tempore soluta aperiam exercitationem incidunt molestiae mollitia nobis eius earum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed sapiente nemo debitis. Cupiditate sit sunt, repellendus nihil quia dolor sint iure voluptate sapiente in harum voluptatem commodi nesciunt iste! Doloremque! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Explicabo reprehenderit ad tempora unde deserunt culpa? Suscipit eos nemo corrupti laudantium illo beatae recusandae maiores praesentium? Tempore harum voluptates consequuntur consequatur!Lorem</p>
-                    </div>
-                    <div className="w-1/2 pl-14 mt-6">
-                        <Image
-                            src="/imagens/sobre.png"
-                            alt="Sobre"
-                            width={500}
-                            height={300}
-                        />
-                    </div>
-                </div>           
+                <div className="w-1/2 text-justify px-8">
+                    <h1 className="text-2xl font-bold text-corSecundaria ">Sobre</h1>
+                    <p className="text-corDestaquePricipal text-lg">Em nossa plataforma, conectamos talentos a oportunidades de trabalho, promovendo um encontro enriquecedor entre candidatos e empresas de todas as regiões. Com o objetivo de transformar a experiência de busca de emprego, nossa solução é desenvolvida com foco em agilidade, acessibilidade e personalização. 
+                    <br />
+                        Nossa missão é tornar a contratação mais simples para empresas e ajudar profissionais de todos os níveis a encontrar vagas ideais, próximas de onde estão. Com uma plataforma intuitiva e filtros regionais, oferecemos uma experiência personalizada, ajudando candidatos a descobrir empregos que se ajustem às suas necessidades locais.
+                        <br />
+                        <br />
+                        Descubra sua próxima oportunidade com a EMPREGUE e faça parte do futuro do mercado de trabalho. Aqui, acreditamos que talento e oportunidade devem estar ao alcance de todos!</p>
+                </div>
+                <div className="w-1/2 pl-14 mt-6">
+                    <Image
+                        src="/imagens/sobre.png"
+                        alt="Sobre"
+                        width={500}
+                        height={300}
+                    />
+                </div>
+            </div>
             <Footer />
         </>
     )
